c49cfe879593d2bb0b0885ebe9b2afaeaa602ebb
JimLiu7434 <jimliu7434@gmail.com>
2015-11-30 08:37:55 +0800

    補推

---
+++ Foods/Class/Material.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Foods.Class
+{
+    public class Material
+    {
+        public string C_MaterialKey { get; set; }
+        public string C_MaterialName { get; set; }
+        public string C_MaterialCatagory { get; set; }
+    }
+}
---
+++ Foods/PageViewModels/DishSubPageViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Foods.Class;
+using Foods.Enum;
+
+namespace Foods.PageViewModels
+{
+	public class DishSubPageViewModel : PageViewModelBase
+	{
+		public DishSubPageViewModel()
+		{
+			var list0 = DumpExcelDataBase.DishList;
+			var list1 = DumpExcelDataBase.SupplierList;
+			var list2 = DumpExcelDataBase.MaterialList;
+
+		}
+	}
+}
