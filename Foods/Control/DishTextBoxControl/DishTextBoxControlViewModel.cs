feee023b8adee1117dbf3649b1e54fa8cde77835
JimLiu <e988lcid@ms55.hinet.net>
2015-12-01 19:12:47 +0800

    開好 usercontrol，尚未完成

--- Foods/Control/DishTextBoxControl/DishTextBoxControlViewModel.cs
+++ Foods/Control/DishTextBoxControl/DishTextBoxControlViewModel.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,22 +6,23 @@ using System.Threading.Tasks;
 using System.Windows.Controls;
 using Foods.Class;
 using Foods.Enum;
+using Foods.PageViewModels;
 
-namespace Foods.PageViewModels
+namespace Foods.Control.DishTextBoxControl
 {
-	public class DishSubPageViewModel : PageViewModelBase
-	{
-	    private string _testText;
+    public class DishTextBoxControlViewModel:PageViewModelBase
+    {
+        private string _testText;
 
         public string TestText
-	    {
+        {
             get { return _testText; }
             set
             {
                 _testText = value;
                 this.OnPropertyChanged("TestText");
             }
-	    }
+        }
 
         private List<AutoCompleteItem> _testItems;
 
@@ -36,19 +36,16 @@ namespace Foods.PageViewModels
             }
         }
 
-        public DishSubPageViewModel()
-		{
-			//var list0 = DumpExcelDataBase.DishList;
-			//var list1 = DumpExcelDataBase.SupplierList;
-			//var list2 = DumpExcelDataBase.MaterialList;
 
-
-
-		}
-
-        public void TextTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        private void TextTextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
-            
+            var list =
+                DumpExcelDataBase.DishList.Where(p => p.C_DishKey.Contains(TestText) || p.C_DishName.Contains(TestText)).ToList();
+
+            if (list.Count > 0)
+            {
+                TestItems = ConvertDishs2AutoCompleteItems(list).ToList();
+            }
         }
 
         private IEnumerable<AutoCompleteItem> ConvertDishs2AutoCompleteItems(List<Dish> list)
@@ -56,14 +53,14 @@ namespace Foods.PageViewModels
             return list.Select(Dish2AutoCompleteItem);
         }
 
-	    private AutoCompleteItem Dish2AutoCompleteItem(Dish dish)
-	    {
-	        return new AutoCompleteItem()
-	        {
-	            HeaderKey = WorkSheetEnum.菜色編號對照,
+        private AutoCompleteItem Dish2AutoCompleteItem(Dish dish)
+        {
+            return new AutoCompleteItem()
+            {
+                HeaderKey = WorkSheetEnum.菜色編號對照,
                 ContentKey = dish.C_DishKey,
                 Value = dish.C_DishName,
-	        };
-	    }
+            };
+        }
     }
 }
